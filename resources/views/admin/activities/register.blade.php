fb3d40b59fb1095e0b83013c058cc70648d5a85d
mldgato <manueldardonhotmail.com>
2024-03-05 10:38:52 -0600

    Actualización 05 de marzo 2024 10:38

--- resources/views/admin/activities/register.blade.php
+++ resources/views/admin/activities/register.blade.php
@@ -22,6 +22,12 @@
 
 @section('js')
     <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
         function onScanSuccess(decodedText, decodedResult) {
             var activity = $('#activity').val();
             var codschool = {{ $activity->id }};
