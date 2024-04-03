5dc0f752a36bf2064993c29409a66d22206b84e3
mldgato <manueldardon@hotmail.com>
2024-04-02 23:27:52 -0600

    Actualización

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,14 +25,95 @@ class UserSeeder extends Seeder
             'email' => 'administracion@colegiodeinfantes.edu.gt',
             'password' => '123456',
         ]);
-        /* User::factory(10)->create();
-
-        $users = User::all();
-        foreach ($users as $user) {
-            $user->activities()->attach([
-                rand(1, 6),
-                rand(7, 11)
-            ]);
-        } */
+        User::create([
+            'name' => 'Edson Alejandro',
+            'surname' => 'Cuéllar Cabrera',
+            'email' => 'eacuellarc@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Juan Carlos',
+            'surname' => 'Bran Morales',
+            'email' => 'jcbranm@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Edmundo Miguel',
+            'surname' => 'Foronda Morales',
+            'email' => 'emforondam@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Ingrid Verónica',
+            'surname' => 'Morales',
+            'email' => 'preprimaria@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Sonia Maricela',
+            'surname' => 'Hernandez Galindo',
+            'email' => 'primaria@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Pamela Fabiola',
+            'surname' => 'Hernández Tejeda',
+            'email' => 'basicos@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Ricardo',
+            'surname' => 'Ortiz Romero',
+            'email' => 'diversificado@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Martina',
+            'surname' => 'Macario Xaper',
+            'email' => 'mmacariox@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Ulices Geovanny',
+            'surname' => 'Castellanos Méndez',
+            'email' => 'ugcastellanosm@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Mynor Vinicio',
+            'surname' => 'Santizo Rosales',
+            'email' => 'mvsantizor@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Hugo Javier',
+            'surname' => 'Castañeda Toca',
+            'email' => 'hjcastanedat@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Carlos Antonio',
+            'surname' => 'Carrillo Mejía',
+            'email' => 'ccarrillo@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Carlos Humberto',
+            'surname' => 'Polanco Flores',
+            'email' => 'chpolancof@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Mario René',
+            'surname' => 'Segura Pastrana',
+            'email' => 'mrsegurap@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
+        User::create([
+            'name' => 'Jaime Armando',
+            'surname' => 'Díaz',
+            'email' => 'jadiaz@colegiodeinfantes.edu.gt',
+            'password' => 'Infantes24',
+        ]);
     }
 }
